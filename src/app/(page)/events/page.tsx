b76d2d7625110c03d8901a088eab509a986a9244
mhmdrazn <razan4944@gmail.com>
2024-11-26 10:44:41 +0700

    fix: typo

--- src/app/(page)/events/page.tsx
+++ src/app/(page)/events/page.tsx
@@ -41,7 +41,7 @@ function Page() {
     },
     {
       subtitle: 'LEADERSHIP BOOTCAMP',
-      shortDesc: 'Program yang berfokus kepada Kepemimpinan dalam meningkatkan kemampuan leadershipnya melalui pelatihan-pelatihan',
+      shortDesc: 'Program yang berfokus kepada Kepemimpinan dalam meningkatkan kemampuan leadershipnya melalui pelatihan-pelatihan.',
       img: '/assets/leadership-bootcamp.png', 
       imgSmall:'/assets/jumlahSiswa.png',
     },
